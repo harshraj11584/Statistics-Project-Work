e9da026948c6e0a54d7345abd2789109b0c89e6e
Harsh <harshraj11584@gmail.com>
2019-08-28 17:32:57 +0530

    prob vals l look better

--- Longitudinal Crossover Designs/Code/H_Algo_bred.m
+++ Longitudinal Crossover Designs/Code/H_Algo_bred.m
@@ -48,33 +48,7 @@ function step_1(H,h_grid_space,theta_vals_l,prob_vals_l,design_l,B_pi_l)
     if psi_max>B_pi_l
         stopping=false;
     end
-            
-    
-%     max_psi = -inf;
-%     theta_argmax_psi = [0.0 0.0 0.0 0.0 0.0];
-%     
-%     for t1=-1:0.1:1
-%         for t2=-1:0.1:1
-%             for t3=-1:0.1:1
-%                 for t4=-1:0.1:1                    
-%                     theta = [t1 t2 t3 t4];
-%                     curr_psi = psi(design_l,theta); 
-%                     curr_psi;
-%                     if max_psi== Inf
-%                         "max_psi is Inf"
-%                     end
-%                     if curr_psi > max_psi
-%                         max_psi = curr_psi
-%                         theta_argmax_psi = theta;
-%                         if max_psi > B_pi_l
-%                             stopping=true;
-%                         end
-%                     end                    
-%                 end
-%             end
-%         end
-%     end
-    
+              
     if stopping==true
         "Found minmax design"
         design_l
@@ -92,15 +66,22 @@ end
 function step_3(H,h_grid_space,theta_vals_l,prob_vals_l,design_l,B_pi_l,theta_l)
     design_l1 = design_l;
     largest_B_pi_t_l1 = -inf;
-    prob_vals_l1 = prob_vals_l;
+    prob_vals_l1 = prob_vals_l
     theta_vals_l1 = theta_vals_l;
     
     theta_vals_l;
     prob_vals_l;
     
-    for t_val=1:1:length(H)
-        theta_vals_t_l1 = [theta_vals_l1; theta_l];
-        prob_vals_t_l1 = [(1-H(t_val)).*prob_vals_l1 H(t_val)];
+    for t_val=2:1:length(H)-1
+        theta_vals_t_l1 = [theta_vals_l; theta_l];
+        %"old"
+        %prob_vals_l1
+        prob_vals_t_l1 = [(1-H(t_val)).*prob_vals_l H(t_val)];
+        %"H(t_val)"
+        %H(t_val)
+        %"new"
+        %prob_vals_t_l1
+        
         design_t_l1 = get_optimal_on_the_average_design(theta_vals_t_l1, prob_vals_t_l1);
         B_pi_t_l1 = B(design_t_l1, theta_vals_t_l1, prob_vals_t_l1);        
         if largest_B_pi_t_l1<B_pi_t_l1
